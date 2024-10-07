fc536fc15b008a9e28357e6f2dfe3d3f1ddf6c1f
五十嵐翔 <igara1119@gmail.com>
2024-10-07 20:15:42 +0900

    clarity追加

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -40,6 +41,15 @@ export default function RootLayout({
 				{children}
 			</body>
 			<GoogleAnalytics gaId="G-Q8EBFQDX3R" />
+			<Script id="clarity-script" strategy="afterInteractive">
+				{`
+(function(c,l,a,r,i,t,y){
+	c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+	t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+	y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "oevkyk9ysa");
+`}
+			</Script>
 		</html>
 	);
 }
