0eda4426e2a421e12284b3ba8579ae31baf85b7e
五十嵐翔 <igara1119@gmail.com>
2024-10-19 12:09:07 +0900

    コミット忘れ

--- data/import.ts
+++ data/import.ts
@@ -1065,7 +1065,7 @@ const youtubeDeckImport = async () => {
 		no: string;
 		name: string;
 	}[] = await Promise.all([
-		...[...Array(122)].map(async (_, index) => {
+		...[...Array(126)].map(async (_, index) => {
 			const i = index + 1;
 			const imagePath = `data/dummy/dummy/${i}.jpg`;
 			return {
