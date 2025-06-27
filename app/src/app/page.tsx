4e1bde9387c4a4c84e43c9d1f248ce4eb6f842e6
五十嵐翔 <igara1119@gmail.com>
2025-06-28 07:29:57 +0900

    チューニング

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,7 @@
 import { Index } from "@/components/pages/index";
-import { Suspense } from "react";
 
 export default function Home() {
 	return (
-		<Suspense fallback={<div>Loading...</div>}>
-			<Index />
-		</Suspense>
+		<Index />
 	);
 }
--- app/src/components/pages/index/index.tsx
+++ app/src/components/pages/index/index.tsx
@@ -1,12 +1,17 @@
+'use client';
+
 import { GeneralTable } from "@/components/pages/index/GeneralTable";
 import { YoutubeDeckProvider } from "@/context/sqlite/YoutubeDeck";
+import { Suspense } from "react";
 
 export const Index: React.FC = () => {
 	return (
 		<YoutubeDeckProvider>
-			<main>
-				<GeneralTable />
-			</main>
+			<Suspense fallback={<div>Loading...</div>}>
+				<main>
+					<GeneralTable />
+				</main>
+			</Suspense>
 		</YoutubeDeckProvider>
 	);
 };
