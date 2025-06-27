d14e8a4ab71eb5b9b9faa4e127e4c8127cf439cb
五十嵐翔 <igara1119@gmail.com>
2025-06-28 03:21:41 +0900

    Suspenseコンポーネントのコメントアウトを解除

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,10 @@
 import { Index } from "@/components/pages/index";
-// import { Suspense } from "react";
+import { Suspense } from "react";
 
 export default function Home() {
 	return (
-		// <Suspense fallback={<div>Loading...</div>}>
+		<Suspense fallback={<div>Loading...</div>}>
 			<Index />
-		// </Suspense>
+		</Suspense>
 	);
 }
